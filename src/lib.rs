892e89e336afe5d764d7ecf04211f2572e5bdd0a
JYC0413 <juyichen0413@gmail.com>
2025-04-14 22:26:25 +0800

    test

--- src/lib.rs
+++ src/lib.rs
@@ -18,45 +18,6 @@ async fn handler(
     body: Vec<u8>,
 ) {
     logger::init();
-
-    // 处理代理请求
-        if _subpath == "/proxy-api" {
-                // 目标 API 的地址
-                let target_url = "http://159.138.158.109:8005/some-endpoint";
-
-                // 使用 reqwest 发送请求到目标 API
-                let client = Client::new();
-                let res = client
-                    .post(target_url)
-                    .json(&qry)  // 如果需要传递参数，使用 .json()
-                    .send()
-                    .await;
-
-                match res {
-                    Ok(ref response) => {  // 使用 ref 来借用 response
-                        // 获取响应体
-                        let body = response.bytes().await.unwrap();
-                        let status_code = response.status().as_u16();
-
-                        // 处理返回响应
-                        send_response(
-                            status_code,
-                            vec![(String::from("content-type"), String::from("application/json"))],
-                            body.to_vec(),
-                        );
-                    }
-                    Err(e) => {
-                        // 错误处理
-                        send_response(
-                            500,
-                            vec![(String::from("content-type"), String::from("text/plain"))],
-                            format!("Error proxying request: {}", e).as_bytes().to_vec(),
-                        );
-                    }
-                }
-            return;
-        }
-
     let (content_type, html) = match _subpath.as_str() {
         "/index.html" | "/index" => ("text/html", include_str!("index.html").as_bytes().to_vec()),
         "/favicon.ico" => ("image/x-icon", include_bytes!("favicon.ico").to_vec()),
